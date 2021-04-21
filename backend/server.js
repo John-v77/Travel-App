41282c89d7fd66b3320b7f6adecb14b847919fce
john-v77 <ionut_vasile85@hotmail.com>
2021-04-21 15:34:28 -0400

    connecting to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 
 const PORT = process.env.PORT || 5000
-// const dotenv = require('dotenv')
-// require('dotenv').config()
 
+const dotenv = require('dotenv')
+require('dotenv').config()
 
+mongoose
+    .connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology:true})
+    .then((x) => console.log(`Connected to Mongo! Database name "${x.connections[0].name}"`))
+    .catch((err) =>  console.log('Error connecting to mongo', err))
 
+app.get('/', (req, res) => {
+    console.log('you made it to the backend')
+})
 
 app.listen(PORT, ()=> console.log('listening to port', PORT))
