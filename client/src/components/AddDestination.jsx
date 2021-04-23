716440f3e3a718712dc9f1050ff5553884c1e05f
john-v77 <ionut_vasile85@hotmail.com>
2021-04-23 11:18:28 -0400

    frontend - added form

---
+++ client/src/components/AddDestination.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import axios from 'axios'
+function AddDestination(props) {
+    const [destination, setDestination] = useState('')
+
+    const handleSubmit =(e)=>{
+        
+        //send it to the server
+        e.preventDefault()
+        axios.post('http//localhost:5000', {destination})
+            .then(console.log)
+            .catch(console.error)
+
+    }
+    const handleChange =(e)=>{setDestination(e.target.value) }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleChange} type="text" name="destination" placeholder="Add a destination ..."/>
+                <button>Add</button>
+            </form>
+        </div>
+    );
+}
+
+export default AddDestination;
