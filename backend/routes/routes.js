6b6b44ee990514410890d3567986a60b0362ebeb
john-v77 <ionut_vasile85@hotmail.com>
2021-04-27 13:22:42 -0400

    bugs fixed from routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const Destinations = require('../model/tripModel')
 
-
-router.post(`/addDestination`, (req, res, next) => { 
+router.post(`/addDestination`, async (req, res, next) => { 
     
     console.log('you made it to the backend', req.body)
-    let newDestination = req.body
-
+    let newDestination = await Destinations.create(req.body)
+    
     res.status(200).json(newDestination)
 })
 
--- backend/server.js
+++ backend/server.js
@@ -2,32 +2,26 @@ const express = require('express')
 const app = express()
 
 const PORT = process.env.PORT || 5000
-const MONGODB_URL = process.env.MONGODB_URL
+
 
 const dotenv = require('dotenv')
 require('dotenv').config()
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/localIronPlate`
 
-const cors = require('cors')
-app.use(
-    cors({credentials:true, 
-          origin:["http://localhost:5000"]
-        })
-)
+// const cors = require('cors')
+// app.use(
+//     cors({credentials:true, 
+//           origin:["http://localhost:5000"]
+//         })
+// )
 
-app.use('/api', require('./routes   '))
-
+app.use('/api', require('./routes/routes'))
 const Destinations = require('./model/tripModel')
 const mongoose = require('mongoose')
 mongoose
-    .connect(MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology:true})
+    .connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology:true})
     .then((x) => console.log(`Connected to Mongo! Database name "${x.connections[0].name}"`))
     .catch((err) =>  console.log('Error connecting to mongo', err))
 
-
-
-
-// app.get('/', (req, res) => {
-//     console.log('you made it to the backend')
-// })
 
 app.listen(PORT, ()=> console.log('listening to port', PORT))
