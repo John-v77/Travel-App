5095b0bd10a9fb5f37bafe50dd94bd644eb92098
john-v77 <ionut_vasile85@hotmail.com>
2021-05-01 17:23:04 -0400

    backend - add a function that encrypts passwords

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -23,7 +25,28 @@ const userSchema = new mongoose.Schema({
     passwordConfirm:{
         type:String,
         required:[true, 'Please confirm your password'],
+        validate:{
+            // This only works on CREATE or SAVE, important when updating users -- 
+            validator: function(el){
+                return el === this.password
+            },
+            message: 'Passwords do not match!'
+        }
     }
+})
+
+userSchema.pre('save', async function(next){
+    //this functions run only if the password was modified of freshly created
+    if(!this.isModified('password')) return next();
+
+    //encript the passwords, at the cpu cost of 12
+    this.password = await bcrypt.hash(this.password, 12)
+
+    //resets the passwordConfirm after validation
+    this.passwordConfirm = undefined
+
+    //move to the next middleware
+    next();
 })
 
 const UserModel = mongoose.model('UserModel', userSchema)
