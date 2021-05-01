2f14f62e676c00ad68a0c02d43cba4cfe19143d0
john-v77 <ionut_vasile85@hotmail.com>
2021-05-01 16:05:12 -0400

    backend - mininal tripRoutes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,19 +3,39 @@ const router = express.Router();
 const Destinations = require('../model/tripModel')
 
 
+
 // Add an item to the data base
 router.post(`/addDestination`, async (req, res, next) => { 
-    
-    console.log('you made it to the backend', req.body)
-    let newDestination = await Destinations.create(req.body)
-    
-    res.status(200).json(newDestination)
+    try{
+        // console.log('you made it to the backend', req.body)
+        let newDestination = await Destinations.create(req.body)
+        res.status(200).json({
+            status:'success',
+            data:{destination: newDestination}
+        })
+    }catch(err){
+        res.status(400).json({
+            status:'failed',
+            message:err
+        })
+    }
 })
 
 // Find all the records in the database and send it to the frontend
 router.get('/AllDestinations', async (req, res, next) => {
-    let allDestinations = await Destinations.find({})
-    res.status(200).json(allDestinations)
+    try{
+        let allDestinations = await Destinations.find({})
+        res.status(200).json({
+            status:'success',
+            results: Destinations.length,
+            data:{allDestinations : allDestinations}
+        })
+    } catch(err){
+        res.status(404).json({
+            status:'failed',
+            message:err
+        })
+    }
 })
 
 
